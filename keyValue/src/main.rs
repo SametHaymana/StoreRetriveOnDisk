5c8928b97d7e549c917088ec93d09b6dda4e4e02
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-02-23 00:55:34 +0300

    Add StorageKV and Disk modules, and retrieve value from disk

--- keyValue/src/main.rs
+++ keyValue/src/main.rs
@@ -1,3 +1,9 @@
+use Storage::{disk::Disk, StorageKV};
+
+mod Storage;
+
 fn main() {
-    println!("Hello, world!");
+    let  disk:StorageKV<String> = StorageKV::new(Box::new(Disk::new("testDir".to_string())));
+
+    println!("{:?}", disk.retrive(&"key1".to_string()).unwrap());
 }
